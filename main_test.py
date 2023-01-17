ac6030debbdd9cb82e22bbe86f5cc7d9acd68593
Kyu Lee <kwooler@gmail.com>
2023-01-17 11:08:16 -0800

    A31

--- main_test.py
+++ main_test.py
@@ -7,16 +7,20 @@ import re
 def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '10\n2\n2\n'
-    sys.stdin = io.StringIO(datastr)
+    # datastr = '10\n2\n2\n'
+    # sys.stdin = io.StringIO(datastr)
 
     main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
+    rvalues = list(map(int, lines[0].split()))
+    print(rvalues)
+    minval = min(rvalues)
     print(lines)
 
-    res = re.search('[\w,\W]*2[\w,\W]*2[\w,\W]*', lines[0])
+    regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
+    res = re.search(regex_string, lines[1])
     assert res != None
     print(res.group())
 
@@ -24,32 +28,19 @@ def test_main_1():
 def test_main_2():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '10\n10\n10\n'
-    sys.stdin = io.StringIO(datastr)
+    # datastr = '10\n2\n2\n'
+    # sys.stdin = io.StringIO(datastr)
 
     main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
+    rvalues = list(map(int, lines[0].split()))
+    print(rvalues)
+    minval = min(rvalues)
     print(lines)
 
-    res = re.search('[\w,\W]*10[\w,\W]*10[\w,\W]*10[\w,\W]*', lines[0])
-    assert res != None
-    print(res.group())
-
-
-def test_main_3():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    datastr = '1\n2\n3\n'
-    sys.stdin = io.StringIO(datastr)
-
-    main.main()
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
-
-    res = re.search('[\w,\W]*distinct[\w,\W]*', lines[0])
+    regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
+    res = re.search(regex_string, lines[1])
     assert res != None
     print(res.group())
