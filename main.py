ce7ee1e91ff9e4e1754b6389973a300abf9e2473
Kyu Lee <kwooler@gmail.com>
2023-05-22 00:59:07 -0700

    Assignment 3-1

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import random
+
+
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-    pass
+    number1 = random.randint(0, 100)
+    number2 = random.randint(0, 100)
+    number3 = random.randint(0, 100)
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
+
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    return number1, number2, number3, min_value
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -10,19 +10,21 @@ def test_main_1():
     # datastr = '10\n2\n2\n'
     # sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    n1, n2, n3, mv = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
-    rvalues = list(map(int, lines[0].split()))
-    print(rvalues)
-    minval = min(rvalues)
     print(lines)
+    minval = min(n1, n2, n3)
+    assert mv == minval
+    # rvalues = list(map(int, lines[0].split()))
+    # print(rvalues)
+    # minval = min(rvalues)
 
-    regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
-    res = re.search(regex_string, lines[1])
-    assert res != None
-    print(res.group())
+    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
+    # res = re.search(regex_string, lines[1])
+    # assert res != None
+    # print(res.group())
 
 
 def test_main_2():
@@ -31,16 +33,17 @@ def test_main_2():
     # datastr = '10\n2\n2\n'
     # sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    n1, n2, n3, mv = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
-    rvalues = list(map(int, lines[0].split()))
-    print(rvalues)
-    minval = min(rvalues)
     print(lines)
+    minval = min(n1, n2, n3)
+    assert mv == minval
+    # rvalues = list(map(int, lines[0].split()))
+    # print(rvalues)
+    # minval = min(rvalues)
 
-    regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
-    res = re.search(regex_string, lines[1])
-    assert res != None
-    print(res.group())
+    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
+    # res = re.search(regex_string, lines[1])
+    # assert res != None
