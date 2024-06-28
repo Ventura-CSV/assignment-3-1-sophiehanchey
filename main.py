2b607c51b7bd2c0e9ec1924f479b3faa327f9f6a
Sophie Hanchey <sophiehanchey@gmail.com>
2024-06-27 18:52:12 -0700

    initial code, pytest passed

--- main.py
+++ main.py
@@ -5,11 +5,21 @@ def main():
     number1 = random.randint(0, 100)
     number2 = random.randint(0, 100)
     number3 = random.randint(0, 100)
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    
+    if(number2<number1):
+        if(number3<number2):
+            min_value = number3
+        else:
+            min_value = number2
+    elif(number3<number1):
+        if(number2<number3):
+            min_value = number2
+        else:
+            min_value = number3
+    else:
+        min_value = number1
+        
+    print(f'{number1}, {number2}, {number3}, minimum: {min_value}')
 
     ########################################
     # Do not delete the return statement
